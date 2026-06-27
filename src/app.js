548261fbfab858773fa95069420138fc56717515
s <wkdrns153@gmail.com>
2026-06-27 23:59:10 +0900

    Add static robots file and Naver verification

--- src/app.js
+++ src/app.js
@@ -1433,6 +1433,7 @@ function htmlPage(pathname, origin, options = {}) {
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="google-adsense-account" content="ca-pub-3386559853644133" />
+  <meta name="naver-site-verification" content="b6e5b11e3e08d55df1d3c09091738722d414eb42" />
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
