2f74268c2efa6bb9dc59664f52586e4b3727b810
s <wkdrns153@gmail.com>
2026-07-02 00:31:50 +0900

    Add deployment debug headers for Crowdpic routing

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import { requestHandler } from '../src/crowdpicWrapper.js';
 
 export default async function handler(req, res) {
+  if (typeof res?.setHeader === 'function') {
+    res.setHeader('x-handler-entry', 'api-index');
+  }
   return requestHandler(req, res);
 }
 
